e6917ea08b3b93339cf8c631ca34f93410e66cb3
Shaw <michele_aliffi@yahoo.it>
2021-11-16 15:07:58 +0100

    Last update

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -13,8 +13,6 @@ public class CalcolaBiglietto {
 		int overAge = 65;
 		int age;
 		
-		
-		
 		System.out.println("Inserisci i km da percorrere: ");
 		double kmDaPercorrere = scanner.nextDouble();
 		String kmFormattati = String.format("%.2f", kmDaPercorrere);
@@ -22,6 +20,20 @@ public class CalcolaBiglietto {
 		
 		System.out.println("Inserire età passeggero: ");
 		age = scanner.nextInt();
+		
+		double kmSom = kmFormattati * baseCost;
+		
+		if(age > overAge) {
+			System.out.println("-40%");
+		} else if (age < underAge) {
+			System.out.println("-20%");
+		} else {
+			System.out.println("No Sconto");
+		}
+		
+		double kmSomAge = kmSom * age;
+		
+		System.out.println("Costo per km ed età: " + kmSomAge);
 		
 		scanner.close();
 		
