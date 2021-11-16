05b6715d4a54b77a67fd4a49bea09178e688f861
Shaw <michele_aliffi@yahoo.it>
2021-11-16 14:42:02 +0100

    Start project

--- src/org/generation/italy/CalcolaBiglietto.java
+++ src/org/generation/italy/CalcolaBiglietto.java
@@ -1,10 +1,30 @@
 package org.generation.italy;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double baseCost = 0.21;
+		int underAge = 17;
+		int overAge = 65;
+		int age;
+		
+		
+		
+		System.out.println("Inserisci i km da percorrere: ");
+		double kmDaPercorrere = scanner.nextDouble();
+		String kmFormattati = String.format("%.2f", kmDaPercorrere);
+		System.out.println(kmFormattati);
+		
+		System.out.println("Inserire età passeggero: ");
+		age = scanner.nextInt();
+		
+		scanner.close();
+		
 	}
 
 }
